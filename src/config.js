e18974f199afb194b7ed8f9a780d49dfcee0c4b3
Kevin Johnson <kevinejohn@gmail.com>
2022-07-11 12:44:07 -0500

    added XEC as ticker option

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const MAGIC_NUMS = {
   BSV: 'e3e1f3e8',
   BCH: 'e3e1f3e8',
   BTC: 'f9beb4d9',
+  XEC: 'e3e1f3e8',
   'BSV-STN': 'fbcec4f9'
 }
 
@@ -9,6 +10,7 @@ const VERSIONS = {
   BSV: 70015,
   BTC: 70015,
   BCH: 70015,
+  XEC: 70015,
   'BSV-STN': 70015
 }
 
@@ -16,6 +18,7 @@ const USER_AGENTS = {
   BSV: '/Bitcoin Atlas:1.0.0/',
   BTC: '/Bitcoin/',
   BCH: '/Bitcoin/',
+  XEC: '/Bitcoin/',
   'BSV-STN': '/Bitcoin SV:0.2.2(EB10000.0)/'
 }
 
